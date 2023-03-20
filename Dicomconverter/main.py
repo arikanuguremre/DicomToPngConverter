596a81bc456c96fceacf04372919db5609d4b7c4
Uğur Emre Arıkan <85636279+arikanuguremre@users.noreply.github.com>
2023-03-21 00:48:07 +0300

    delete redundant library and code part

--- Dicomconverter/main.py
+++ Dicomconverter/main.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pydicom
-import cv2 as cv
 from PIL import Image
-from pydicom.data import get_testdata_file
+
 
 
 #pydicom part
@@ -14,14 +13,12 @@ print(ds)
 pixel_data = pydicom.pixel_data_handlers.apply_windowing(pixel_data,ds,0)
 
 
-# Normalize the pixel values to the range [0, 65535] again
-#normalized_pixel_data = (pixel_data - np.min(pixel_data)) / np.ptp(pixel_data) * 65535.0
+# Normalize the pixel values to the range [0, 65535] 
 pixel_data = (pixel_data / np.max(pixel_data)) * 65535.0
 
 # Convert the pixel values to the uint16 data type
 pixel_data = pixel_data.astype('uint16')
-#cv.imshow("Converted 16-bit depth png",pixel_data)
-#cv.waitKey(0)
+
 #save 
 image = Image.fromarray(pixel_data)
 image.save("newconverteddcm.png")
